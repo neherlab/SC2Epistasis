c0076048d7b8b5cbad551e90cbf75bf7ec288fbe
Luca Sesta <lucasesta95@gmail.com>
2025-09-19 15:16:09 +0200

    Packages JSON and HypothesysTests added to dependencies

--- src/SC2Epistasis.jl
+++ src/SC2Epistasis.jl
@@ -1,5 +1,7 @@
 module SC2Epistasis
 
-using DataFrames, LinearAlgebra, NLopt, PdbTool, BioAlignments, Statistics, Combinatorics
+using DataFrames, LinearAlgebra, NLopt, PdbTool, BioAlignments, Statistics, Combinatorics, JSON, CSV, HypothesisTests
+
+include("clade_diff.jl")
 
 end # module SC2Epistasis
