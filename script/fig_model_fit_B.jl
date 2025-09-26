2a89369a3e02d9bc685eadf9e9147f8b23fe4850
Luca Sesta <lucasesta95@gmail.com>
2025-09-26 14:13:38 +0200

    Script for producing and saving model-fit figure

---
+++ script/fig_model_fit_B.jl
@@ -0,0 +1,58 @@
+""" Script to generate panel B of figure 4 """
+
+# Import packages
+using DataFrames, CSV, PyPlot
+
+# Plotting functions
+
+function plot_model_fit(fit_df_vec::Vector{DataFrame}, e0::Float64;
+    labels::Vector{String}=["Uniform", "3d", "3d-sigmoid"],
+    linestyles::Vector{String}=["-", "--", "-."],
+    shades1::Vector{NTuple{4,Float64}}=[get_cmap("Blues")(0.4 + 0.2 * i) for i in 0:2],
+    shades2::Vector{NTuple{4,Float64}}=[get_cmap("Reds")(0.4 + 0.2 * i) for i in 0:2])
+
+    @assert length(fit_df_vec) == length(labels) == length(linestyles) == length(shades1) == length(shades2)
+
+    fig, ax = subplots(1, 2, figsize=(10, 5))
+
+    for n in eachindex(fit_df_vec)
+        fit_df = fit_df_vec[n]
+        ax[1].plot(fit_df.l1, fit_df.energy, marker=".", markersize=5.0,
+            linestyle=linestyles[n], color=shades1[n], label=labels[n])
+    end
+
+    ax[1].hlines(e0, fit_df_vec[end].l1[1], fit_df_vec[end].l1[end], color="black", linestyle="-", label="Estimated noise")
+    ax[1].set_xscale("log")
+    ax[1].set_xlabel("λ₁", fontsize=16)
+    ax[1].set_ylabel("Energy", fontsize=14)
+    ax[1].legend(loc="best", fontsize=12)
+    for n in eachindex(fit_df_vec)
+        fit_df = fit_df_vec[n]
+        ax[2].plot(fit_df.l1, 1 .- fit_df.tot_zf, marker=".", markersize=5.0,
+            linestyle=linestyles[n], color=shades2[n], label=labels[n])
+    end
+    ax[2].set_xscale("log")
+    ax[2].set_yscale("log")
+    ax[2].set_xlabel("λ₁", fontsize=16)
+    ax[2].set_ylabel("Non-zero fraction", fontsize=14)
+    ax[2].legend(loc="best", fontsize=12)
+    fig.tight_layout()
+    fig.savefig("results/figures/fig_model_fit_B.pdf", dpi=500)
+    fig.close()
+
+end
+
+##############################--------------------------------
+
+# Script body
+
+# Read dataframes with model fits
+path_model_fit = "results/model_fit/" # path to folder with model fit results
+files = readdir(path_model_fit, join=true) # list of files in the folder
+fit_df_vec = [DataFrame(CSV.File(f)) for f in files]; # vector of dataframes
+
+# Compute baseline noise level
+include("../script/usa_uk_ener.jl") # e0 is defined here
+
+# Plot model fits
+plot_model_fit(fit_df_vec, e0);
