722eee1d38527828cff4c6e20875fb999412b429
Richard Neher <richard.neher@unibas.ch>
2026-06-12 14:17:57 +0200

    refactor plotting of raw_epi

--- script/fig_raw_signal_B.jl
+++ script/fig_raw_signal_B.jl
@@ -2,61 +2,7 @@
 
 # Import packages
 using DataFrames, CSV, JLD2, PyPlot, PdbTool
-
-# Plotting function
-function plot_sphere_frac(cp_plt::Vector{Tuple{S1,S1}}, frac::Vector{Matrix{Float64}}, rnd_frac::Vector{Vector{Float64}};
-    radii::Vector{Float64}=[5.0, 8.0, 10.0, 12.0, 15.0, 20.0],
-    z_thr::Vector{Vector{Float64}}=[[1.75, 2.0, 2.25], [1.25, 1.5, 1.75], [1.5, 1.75, 2.0], [1.5, 1.75, 2.0]]) where {S1<:AbstractString}
-
-    n = length(cp_plt)  # number of clade pairs to be plotted
-
-    fig, axs = subplots(n, 1, figsize=(4.5, 3.5 * n), sharex=true)
-    if n == 1
-        axs = [axs]
-    end
-
-    for (i, cpair) in enumerate(cp_plt)
-        ax = axs[i]
-
-        # Plot fraction in sphere and random benchmark
-        for k in eachindex(z_thr[i])
-            ax.plot(radii, frac[i][:, k], ".-")
-        end
-        ax.plot(radii, rnd_frac[i], ".-")
-
-        # Subplot clade pair as y-axis label
-        ax.set_ylabel(cpair[1] * "-" * cpair[2], fontsize=14, labelpad=18, rotation=90, ha="left", va="center")
-
-        # Move y-tick labels to the right and increase fontsize
-        ax.yaxis.tick_right()
-        ax.tick_params(axis="y", labelsize=11)
-
-        # set common y-axis limit
-        ax.set_ylim(0.0, 1.05)
-
-        # Subplot legend
-        ax.legend(vcat("z ≥ " .* string.(z_thr[i]), "Random"), fontsize=13)
-
-        # only clear tick labels for non-bottom subplots
-        if i != n
-            ax.tick_params(labelbottom=false)
-        end
-
-    end
-
-    # Then adjust margins to accommodate the common y-axis label and right-side labels
-    fig.tight_layout(rect=(0.02, 0.025, 0.87, 0.98))
-
-    # common y-axis label on the right side
-    fig.text(0.92, 0.5, "Fraction of " * L"i\;" * "s.t. " * L"\exists j:\:d(i,j) < d_{thr}", va="center", rotation="vertical", fontsize=16)
-
-    # bottom x-axis
-    axs[end].set_xlabel("Sphere radius (Å)", fontsize=16)
-    axs[end].tick_params(axis="x", labelsize=11)
-
-    return fig, axs
-
-end
+include("z_plot_sphere_frac.jl")
 
 function plot_sphere_frac_vs_z(cp_plt::Vector{Tuple{S1,S1}}, frac_z::Vector{Matrix{Float64}},
     z_thr_range::Vector{Float64}, rnd_frac::Vector{Vector{Float64}};
@@ -142,7 +88,7 @@ close(fig)
 
 # --- Fraction vs z-score for two fixed radii ---
 radii_sel = [8.0, 15.0]
-z_thr_range = collect(0.5:0.1:3.0)
+z_thr_range = collect(0.5:0.1:2.0)
 
 frac_vec_z = [zeros(Float64, length(radii_sel), length(z_thr_range)) for i in eachindex(cp_plt)]
 rnd_frac_z = [zeros(Float64, length(radii_sel)) for i in eachindex(cp_plt)]
--- script/si_fig_s2_B.jl
+++ script/si_fig_s2_B.jl
@@ -2,61 +2,7 @@
 
 # Import packages
 using DataFrames, CSV, JLD2, PyPlot, PdbTool
-
-# Plotting function
-function plot_sphere_frac(cp_plt::Vector{Tuple{S1,S1}}, frac::Vector{Matrix{Float64}}, rnd_frac::Vector{Vector{Float64}};
-    radii::Vector{Float64}=[5.0, 8.0, 10.0, 12.0, 15.0, 20.0],
-    z_thr::Vector{Vector{Float64}}=[[1.75, 2.0, 2.25], [1.25, 1.5, 1.75], [1.5, 1.75, 2.0], [1.5, 1.75, 2.0]]) where {S1<:AbstractString}
-
-    n = length(cp_plt)  # number of clade pairs to be plotted
-
-    fig, axs = subplots(n, 1, figsize=(4.5, 5.4 * n), sharex=true, layout="constrained")
-    if n == 1
-        axs = [axs]
-    end
-
-    for (i, cpair) in enumerate(cp_plt)
-        ax = axs[i]
-
-        # Plot fraction in sphere and random benchmark
-        for k in eachindex(z_thr[i])
-            ax.plot(radii, frac[i][:, k], ".-")
-        end
-        ax.plot(radii, rnd_frac[i], ".-")
-
-        # Subplot clade pair as y-axis label
-        ax.set_ylabel(cpair[1] * "-" * cpair[2], fontsize=14, labelpad=18, rotation=90, ha="left", va="center")
-
-        # Move y-tick labels to the right and increase fontsize
-        ax.yaxis.tick_right()
-        ax.tick_params(axis="y", labelsize=11)
-
-        # set common y-axis limit
-        ax.set_ylim(0.0, 1.05)
-
-        # Subplot legend
-        ax.legend(vcat("z ≥ " .* string.(z_thr[i]), "Random"), fontsize=13)
-
-        # only clear tick labels for non-bottom subplots
-        if i != n
-            ax.tick_params(labelbottom=false)
-        end
-
-    end
-
-    # Then adjust margins to accommodate the common y-axis label and right-side labels
-    fig.tight_layout(rect=(0.02, 0.025, 0.87, 0.98))
-
-    # common y-axis label on the right side
-    fig.text(0.92, 0.5, "Fraction of " * L"i\;" * "s.t. " * L"\exists j:\:d(i,j) < d_{thr}", va="center", rotation="vertical", fontsize=16)
-
-    # bottom x-axis
-    axs[end].set_xlabel("Sphere radius (Å)", fontsize=16)
-    axs[end].tick_params(axis="x", labelsize=11)
-
-    return fig, axs
-
-end
+include("z_plot_sphere_frac.jl")
 
 ###############################---------------------------------
 
